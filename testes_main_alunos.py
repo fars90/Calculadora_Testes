ddcec0d4bd1e6892ab2444b1cdd2a6adebf2e9cc
fars90 <andreraposeirofaria@gmail.com>
2025-07-04 12:30:54 +0100

    Update testes_main_alunos.py

--- testes_main_alunos.py
+++ testes_main_alunos.py
@@ -8,11 +8,21 @@ class TestCalculadora(unittest.TestCase):
     def teste_operacoes_basicas(self):
         # Teste operações básicas de cada operador + - * / % ^
         self.assertEqual(calculadora(2, 3, '+'), 5)
+        self.assertEqual(calculadora(5, 2, '-'), 3)
+        self.assertEqual(calculadora(2, 3, '*'), 6)
+        self.assertEqual(calculadora(4, 2, '/'), 2)
+        self.assertEqual(calculadora(5, 2, '%'), 1)
+        self.assertEqual(calculadora(3, 2, '^'), 6)
 
     def teste_v2_operacoes(self):
         # Teste operações básicas de cada operador + - * / % ^
         self.assertEqual(calculadora_v2(2, 3, '+'), 5)
-
+        self.assertEqual(calculadora_v2(5, 2, '-'), 3)
+        self.assertEqual(calculadora_v2(2, 3, '*'), 6)
+        self.assertEqual(calculadora_v2(4, 2, '/'), 2)
+        self.assertEqual(calculadora_v2(5, 2, '%'), 1)
+        self.assertEqual(calculadora_v2(3, 2, '^'), 6)
+        
     def teste_v3_operacoes(self):
         # Teste operações básicas de cada operador + - * / % ^
         self.assertEqual(calculadora_v3(2, 3, '+'), 5)
